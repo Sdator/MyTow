461baed7ba73ab0108b9b34c143b5a4bea697e91
Sdator <250740270@qq.com>
2019-06-07 17:47:39 +0800

    兼容新页面

--- Js/暴力猴/畅读搜狐.user.js
+++ Js/暴力猴/畅读搜狐.user.js
@@ -3,7 +3,7 @@
 // @author              絕版大叔丶
 // @namespace           https://github.com/Sdator/MyCode/tree/master/Js/%E6%9A%B4%E5%8A%9B%E7%8C%B4
 // @icon                https://avatars3.githubusercontent.com/u/17621623?s=40&v=4
-// @version             1.0
+// @version             1.1
 // @match               *://www.sohu.com/a/*
 // @description         删除搜狐所有广告，重建主题内容
 // @updateURL           https://github.com/Sdator/MyCode/raw/master/Js/%E6%9A%B4%E5%8A%9B%E7%8C%B4/畅读搜狐.user.js
