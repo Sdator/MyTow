f3924c52698a593e0f3c16e36f73ea88dde40df1
Sdator <250740270@qq.com>
2020-08-02 06:36:39 +0800

    接口完成

--- Js/暴力猴/去广告类/网易课堂.user.js
+++ Js/暴力猴/去广告类/网易课堂.user.js
@@ -5,7 +5,9 @@
 // @icon
 // @version             1.0
 // @description         网易云课堂去广告
-// @match               *://study.163.com/course/courseLearn.htm?*
+// @match               *://study.163.com/course/courseLearn.htm*
+// @match               *://study.163.com/course/introduction/*.htm*
+// @match               *://study.163.com/course/courseMain.htm*
 //更新地址
 // @updateURL
 // @match               输入匹配网页
@@ -23,22 +25,21 @@
 });
 
 class A {
-    async get课程() {
+    async get课程(视频列表id) {
         const time = new Date().getTime()
-        const 视频列表id = 1512007
-
+        if (!视频列表id) throw new Error("没有id")
         // 需要变动
-        const body = `callCount=1
-scriptSessionId=\${scriptSessionId}190
-httpSessionId=3f847fddcc124945b1a2148bd4839f41
-c0-scriptName=PlanNewBean
-c0-methodName=getPlanCourseDetail
-c0-id=0
-c0-param0=string:${视频列表id}
-c0-param1=number:0
-c0-param2=null:null
-batchId=${time}`
-
+        const body =
+            `callCount=1
+            scriptSessionId=\${scriptSessionId}190
+            httpSessionId=3f847fddcc124945b1a2148bd4839f41
+            c0-scriptName=PlanNewBean
+            c0-methodName=getPlanCourseDetail
+            c0-id=0
+            c0-param0=string:${视频列表id}
+            c0-param1=number:0
+            c0-param2=null:null
+            batchId=${time}`
         // 视频列表
         const url = `https://study.163.com/dwr/call/plaincall/PlanNewBean.getPlanCourseDetail.dwr?${time}`
         const rev = await fetch(url, {
@@ -46,12 +47,12 @@ batchId=${time}`
             body: body
         })
         const txt = await rev.text()
-        //      取前部分字符串
-        //         const newtxt = txt.slice(0, txt.indexOf('dwr.engine._remoteHandleCallback'))
-        //       执行字符串  随便加入一个全局变量设置好拿到返回值
-        const listData = eval(txt + `window['aaa']=s3;`)
-
-        console.log(listData, 111111)
+        // 取前部分字符串
+        // const newtxt = txt.slice(0, txt.indexOf('dwr.engine._remoteHandleCallback'))
+        // 执行字符串  随便加入一个全局变量设置 好拿到返回值
+        const listData = eval(txt + `window['aaa']=s3;window['bbb']=s0;`)
+        // 写到本地
+        // localStorage.api = JSON.stringify(listData)
 
         let i = 0
         //      记录课程id和名称 用于返回
@@ -67,8 +68,61 @@ batchId=${time}`
         }
         return data
     }
+    async get视频信息(视频ID, 列表ID) {
+        const url = `https://study.163.com/dwr/call/plaincall/LessonLearnBean.getVideoLearnInfo.dwr?${new Date().getTime()}`
+        const body =
+            `callCount=1
+scriptSessionId=\${scriptSessionId}190
+httpSessionId=3f847fddcc124945b1a2148bd4839f41
+c0-scriptName=LessonLearnBean
+c0-methodName=getVideoLearnInfo
+c0-id=0
+c0-param0=string:${视频ID}
+c0-param1=string:${列表ID}
+batchId=1596230378140
+`
+        const rev = await fetch(url, {
+            method: 'POST',
+            headers: {
+                // 补全原有请求的信息 尽量相同
+                "providerid": window.ownerId,
+                "content-type": "text/plain"
+            },
+            body: body
+        })
+        const txt = await rev.text()
+        const videoInfo = eval(txt + ";window[s1]=s1")
+        return await this.get视频地址(videoInfo.videoId, videoInfo.signature)
+    }
 
-    // 取源码
+    async get视频地址(视频ID, 视频key) {
+        const body = {
+            videoId: 视频ID,
+            signature: 视频key,
+            clientType: 1
+        }
+
+        const url = new URL("https://vod.study.163.com/eds/api/v1/vod/video")
+        // 构建url
+        for (const v of Object.entries(body)) {
+            url.searchParams.append(...v)
+        }
+        const rev = await fetch(url, {
+            method: 'get',
+            credentials: 'include',
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+
+            }
+        })
+        const txt = await rev.text()
+        console.log(txt, 2222222);
+    }
+
+    /**
+     * 获取页面源码 返回dom节点
+     * @param {string} url
+     */
     async get源码(url) {
 
         // 1 读取网站源码 如果请求状态不是200-299 抛出异常并输出
@@ -88,9 +142,11 @@ batchId=${time}`
         return el
     }
 
-    // 取源码
-    async get视频() {
-        const id = await this.get课程()
+
+
+    // 封存 直接改用调用接口
+    async get视频(视频列表ID) {
+        const id = await this.get课程(视频列表ID)
         const data = []
         let i = 0
         for (const { 课程ID } of id) {
@@ -114,7 +170,25 @@ batchId=${time}`
 }
 
 const a = new A()
-a.get视频();
+try {
+    const 视频列表ID = ["1512007", "1002895002"]
+    // 获取匹配到的 id 没拿到就抛出错误
+    // let 视频列表id
+    // for (const v of 视频列表) {
+    //     if (location.href.includes(v)) {
+    //         视频列表id = v
+    //         break
+    //     }
+    // }
+
+    // a.get视频(视频列表ID[0]);
+    a.get视频信息(1802061, 1512007)
+
+
+} catch (error) {
+    console.error(error);
+}
+
 
 
 
